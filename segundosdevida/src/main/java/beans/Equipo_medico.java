ad826b9d420cd09196382449c698747bf5af48c9
fundacion <pfundacionsegundosdevida@gmail.com>
2022-09-03 17:34:21 -0500

    CReando el constructor, los get y set y el string() de la clase equipos_medicos

--- segundosdevida/src/main/java/beans/Equipo_medico.java
+++ segundosdevida/src/main/java/beans/Equipo_medico.java
@@ -17,10 +17,75 @@ public class Equipo_medico {
     private int cantidad_equipo;
     private boolean observacion_equipo;
     
-    //--------------------------------------------
+    //------CONSTRUCTOR--------------------------------------
     
-    //--------------------------------------------
     
-    //--------------------------------------------
+
+    public Equipo_medico(int id_equipo, String nombre_equipo, String tipo_equipo, String marca_equipo, int cantidad_equipo, boolean observacion_equipo) {
+        this.id_equipo = id_equipo;
+        this.nombre_equipo = nombre_equipo;
+        this.tipo_equipo = tipo_equipo;
+        this.marca_equipo = marca_equipo;
+        this.cantidad_equipo = cantidad_equipo;
+        this.observacion_equipo = observacion_equipo;
+    }
+        
+    
+    //--------GET Y SET-----------------------------------
+
+    public int getId_equipo() {
+        return id_equipo;
+    }
+
+    public void setId_equipo(int id_equipo) {
+        this.id_equipo = id_equipo;
+    }
+
+    public String getNombre_equipo() {
+        return nombre_equipo;
+    }
+
+    public void setNombre_equipo(String nombre_equipo) {
+        this.nombre_equipo = nombre_equipo;
+    }
+
+    public String getTipo_equipo() {
+        return tipo_equipo;
+    }
+
+    public void setTipo_equipo(String tipo_equipo) {
+        this.tipo_equipo = tipo_equipo;
+    }
+
+    public String getMarca_equipo() {
+        return marca_equipo;
+    }
+
+    public void setMarca_equipo(String marca_equipo) {
+        this.marca_equipo = marca_equipo;
+    }
+
+    public int getCantidad_equipo() {
+        return cantidad_equipo;
+    }
+
+    public void setCantidad_equipo(int cantidad_equipo) {
+        this.cantidad_equipo = cantidad_equipo;
+    }
+
+    public boolean isObservacion_equipo() {
+        return observacion_equipo;
+    }
+
+    public void setObservacion_equipo(boolean observacion_equipo) {
+        this.observacion_equipo = observacion_equipo;
+    }
+    //------------TO STRING()--------------------------------
+
+    @Override
+    public String toString() {
+        return "Equipo_medico{" + "id_equipo=" + id_equipo + ", nombre_equipo=" + nombre_equipo + ", tipo_equipo=" + tipo_equipo + ", marca_equipo=" + marca_equipo + ", cantidad_equipo=" + cantidad_equipo + ", observacion_equipo=" + observacion_equipo + '}';
+    }
+    
     
 }
